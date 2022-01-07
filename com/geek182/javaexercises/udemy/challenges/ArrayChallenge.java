d49839d4bab9058e715aa577c3ffdd67ab6d645d
Leandro Azevedo <leandro.s.azevedo@gmail.com>
2022-01-07 13:11:00 +0100

    Updated with instructor version and comments

--- com/geek182/javaexercises/udemy/challenges/ArrayChallenge.java
+++ com/geek182/javaexercises/udemy/challenges/ArrayChallenge.java
@@ -2,8 +2,8 @@ package com.geek182.javaexercises.udemy.challenges;
 
 /*
 Create a program using arrays that sort a list of integers in descending order.
-if you have an array 106, 80, 101, 12, 1, 0 it should returns
-0, 1, 12, 80, 101, 106
+if you have an array 0, 1, 12, 80, 101, 106  it should return
+106, 80, 101, 12, 1, 0
 */
 
 import java.util.Scanner;
@@ -50,6 +50,37 @@ public class ArrayChallenge{
         }
         return SortedArray;
     }
+
+    /*
+    Below the instructor version:
+    Of course, it is simpler than mine version :)
+    */
+
+    public static int[] sortIntegers(int[] array){
+        int[] sortedArray = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag = false;
+            for(int i=0; i<sortedArray.length-1; i++){
+                if(sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+        }
+
+    /*
+      End of the instructor version
+     */
+
     public void PrintSortedArray(int[] array){
         int[] result = this.SortArrayDesc(array); //Calls Method SortArray()
         for (int i = 0; i < result.length; i++) {
@@ -74,4 +105,14 @@ public class ArrayChallenge{
         */
         ArrayChallenge myarray = new ArrayChallenge(5);
     }
+    /*
+    Checking the instructor version afterwards it becomes clear that I could use static methods so won't be required
+    to instantiate them on the main method.
+    Something like:
+
+    int[] myIntegers = GetInput(5);
+    int[] sorted = sortIntegers(myIntegers);
+    PrintSortedArray(sorted);
+
+     */
 }
